f6f9c4f4640630fb2df8c1d625f28a28eb2cb35b
tejashjoshi11 <tejash.joshi@hotmail.com>
2024-07-25 16:12:53 +0530

    Update main.tf

--- Multiple_EC2/main.tf
+++ Multiple_EC2/main.tf
@@ -1,7 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "terraform"
+}
+
 resource "aws_instance" "example_server" {
   ami           = "ami-0162fe8bfebb6ea16"
   instance_type = "t2.micro"
-  count         = 2
 
   tags = {
     Name = "Tejash Joshi"
