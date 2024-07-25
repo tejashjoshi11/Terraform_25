2b15be70297aaaf02de7745d4693406dbf21a925
tejashjoshi11 <tejash.joshi@hotmail.com>
2024-07-25 16:29:01 +0530

    Update main.tf

--- Multiple_EC2/main.tf
+++ Multiple_EC2/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "example_server" {
   ami           = "ami-0162fe8bfebb6ea16"
   instance_type = "t2.micro"
 
